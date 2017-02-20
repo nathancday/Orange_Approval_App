3c16f70872d3d2a02692c8c7b32775c4542a845c
Nate <nathancday@gmail.com>
2017-02-19 20:49:24 -0500

    weekend work

---
+++ app.R
@@ -0,0 +1,239 @@
+library(forcats)
+library(rvest)
+library(plotly)
+library(lubridate)
+library(tidyverse)
+library(magrittr)
+library(shiny)
+library(shinythemes)
+library(ggplot2)
+
+theme_set(theme_classic(base_size = 18) +
+              theme(panel.border = element_rect(colour = "grey90", fill=NA, size=1),
+                    plot.background = element_rect(fill = "#4e5d6c"),
+                    strip.background = element_rect(linetype = "blank", fill = "#3e444c"),
+                    text = element_text(color = "white", size = rel(.75)),
+                    plot.margin = margin(1,1,1,1, "cm"),
+                    axis.text = element_text(color = "white"),
+                    axis.ticks = element_line(color = "white"),
+                    panel.background = element_rect(fill = "lightgrey"),
+                    panel.grid.minor = element_line(colour="white", size=0.5),
+                    panel.grid.major = element_line(colour="white", size=0.5),
+                    legend.position = "none")
+)
+
+setwd("~/future/President_Approval/Pres_Approval_App/")
+#### UI ####
+ui <- fluidPage(theme = shinytheme("superhero"),
+                titlePanel(title = "Red, White and Boo",
+                           tags$head(tags$link(rel = "shortcut icon", href = "www/favicon.png"))
+                           ),
+                fluidRow(column(12,
+                         p("A loess look at the 🍊's historically low approval ratings with Shiny..."))
+                         ),
+                fluidRow(column(12,
+                                sidebarLayout(sidebarPanel(width = 3,
+                                    h4("Time Scale:"),
+                                    checkboxInput("don_bool", "Focus on first 100 days?", T),
+                                    checkboxInput("don_predict", "Plot projections for 🍊's first 100 days?", F),
+                                    conditionalPanel(condition = "input.don_bool == false",
+                                        sliderInput("time_int", "Select Days in Office:", 100, 2922, value = 1461)
+                                        ),
+                                    br(),
+                                    h4("Presidents Included:"),
+                                    sliderInput("term_int", "Years of Service", as.Date("1940-01-01"), as.Date("2017-12-31"),
+                                                                                       c(as.Date("1940-01-01"), as.Date("2017-12-31")),
+                                                                                       timeFormat = "%Y"),
+                                        
+                                    br(),
+                                    checkboxInput("more_bool", "Show more filters"),
+                                    conditionalPanel(condition = "input.more_bool == true",
+                                        h5("Background Filters:"),
+                                        selectInput("term_select", "Select # of terms", c(1,2)),
+                                        checkboxInput("party_bool", "Filter by party:"),
+                                        selectInput("party_select", "Party:", c("Republican", "Democrat"))
+                                    )
+                                ),
+                                mainPanel(width = 9,
+                                    tabsetPanel(id = "main_tabs",
+                                        tabPanel("", icon = icon("thumbs-up"), value = "approve",
+                                                 plotlyOutput("main_approval_plot", height = "400px"),
+                                                 br(),
+                                                 fluidRow(
+                                                     column(9,
+                                                           tags$body(p("A second plot by Individual Presidents"))
+                                                           )
+                                                     ),
+                                                 plotlyOutput("second_approval_plot", height = "400px")
+                                                 ),
+                                        tabPanel("",icon = icon("thumbs-down"), value = "disapprove",
+                                                 plotlyOutput("main_disapproval_plot", height = "400px"),
+                                                 br(),
+                                                 fluidRow(
+                                                     column(9,
+                                                            tags$body(p("A second plot by Individual Presidents"))
+                                                     )
+                                                 ),
+                                                 plotlyOutput("second_disapproval_plot", height = "400px")
+                                                 ),
+                                        tabPanel("", icon = icon("question"), value = "unsure",
+                                                 plotlyOutput("main_unsure_plot", height = "400px"),
+                                                 br(),
+                                                 fluidRow(
+                                                     column(9,
+                                                            tags$body(p("A second plot by Individual Presidents"))
+                                                     )
+                                                 ),
+                                                 plotlyOutput("second_unsure_plot", height = "400px")
+                                                 )
+                                )
+                                )
+                )
+                )
+                ),
+                br(),
+                fluidRow(column(12,
+                        tabsetPanel(
+                            tabPanel("Data", icon = icon("folder-open"),
+                                     tags$div(class = "info",
+                                              tags$h4("Data"),
+                                              tags$p("The data displayed here is sourced form UC Santa Barbara's",
+                                                     tags$a(href = "http://www.presidency.ucsb.edu/data/popularity.php", "American Presidency Project"), "."),
+                                              tags$p("It contains Gallup's approval poll ratings since Harry S. Truman. Polls have increase in their frequency
+                                                     since Truman and Barack Obama's preseidency ave 2,000+ recorded obeservations, compared to 260 for George W. Bush.
+                                                     Obama's polling obervations have been filterd, by selecting every 10th one, to reduce his impact on the historical averages.")
+                                              )
+                                              ),
+                            tabPanel("Code", icon = icon("github"),
+                                     p("Coming soon...")
+                                     )
+                            )
+                            )
+                                     
+                )
+                )
+
+#### Server ####
+server <- function(input, output) {
+    data <- readRDS("www/data.RDS")
+    trump_proj <- readRDS("www/projected_trump.RDS")
+    trump <- filter(data, president == "Donald J. Trump")
+    data %<>% filter(president != "Donald J. Trump")
+    
+    in_trump <- reactive({
+        if(input$don_predict) {
+            return(trump_proj)
+        }
+        else {
+            return(trump)
+        }
+    })
+    
+    in_data <- reactive({
+        # Don time axis react
+        if(input$don_bool) {
+            data <- filter(data, days_in_office <= 100)
+            }
+        
+        data %<>% filter(days_in_office <= input$time_int)
+        
+        data %<>% filter(as.Date(end_date) > input$term_int[1],
+                         as.Date(end_date) < input$term_int[2])
+        
+        if(input$party_bool) {
+            data %<>% filter(party == input$party_select)
+        }
+        return(data)
+     })
+    
+    proj <- readRDS("www/proj.RDS")
+    proj %<>% filter(president != "Donald J. Trump")
+    in_proj <- reactive({
+        # Don time axis react
+        if(input$don_bool) {
+            proj <- filter(proj, days_in_office <= 100)
+        }
+        
+        proj %<>% filter(days_in_office <= input$time_int)
+        
+        proj %<>% filter(as.Date(end_date) > input$term_int[1],
+                         as.Date(end_date) < input$term_int[2])
+        
+        if(input$party_bool) {
+            proj %<>% filter(party == input$party_select)
+        }
+        return(proj)
+    })
+    
+    output$main_approval_plot <- renderPlotly({
+        p <- ggplot(in_data(), aes(x = days_in_office, y = approval, color = party)) +
+            stat_smooth(se = F, size = 3, n = max(trump$days_in_office)) +
+            stat_smooth(data = in_trump(), size = 3, se = F) +
+            scale_color_manual(values = c("Democrat" = "#232066", "Republican" = "#e91d0e", "Orange" = "orange")) +
+            theme(legend.position = "none") +
+            labs(x = "Days in Office",
+                 y = "Approval Rating")
+        
+        ggplotly(p,originalData = T, tooltip = c("approval", "party"), layer = 1)
+    })
+    
+    output$second_approval_plot <- renderPlotly({
+        
+        p <- ggplot(in_proj(), aes(x = days_in_office, y = approval, group = president, color = party)) +
+            stat_smooth(se = F, aes(linetype = line_type)) +
+            stat_smooth(data = in_trump(), se = F, size = 1, color = "orange") +
+            scale_color_manual(values = c("Democrat" = "#232066", "Republican" = "#e91d0e", "Orange" = "orange")) +
+            theme(legend.position = "none") +
+            labs(x = "Days in Office",
+                 y = "Approval Rating")
+        
+        ggplotly(p,originalData = T, tooltip = c("approval", "president", "party"), layer = 1)
+    })
+    
+    output$main_disapproval_plot <- renderPlotly({
+        p <- ggplot(in_data(), aes(x = days_in_office, y = disapproval, color = party)) +
+            stat_smooth(se = F, size = 3) +
+            stat_smooth(data = in_trump(), size = 3, se = F) +
+            scale_color_manual(values = c("Democrat" = "#232066", "Republican" = "#e91d0e", "Orange" = "orange")) +
+            theme(legend.position = "none") +
+            labs(x = "Days in Office",
+                 y = "Disapproval Rating")
+        ggplotly(p,originalData = T, tooltip = c("disapproval", "party"), layer = 1)
+    })
+    
+    output$second_disapproval_plot <- renderPlotly({
+        p <- ggplot(in_proj(), aes(x = days_in_office, y = disapproval, group = president, color = party)) +
+            stat_smooth(se = F, aes(linetype = line_type)) +
+            stat_smooth(data = in_trump(), se = F, size = 1, color = "orange") +
+            scale_color_manual(values = c("Democrat" = "#232066", "Republican" = "#e91d0e", "Orange" = "orange")) +
+            theme(legend.position = "none") +
+            labs(x = "Days in Office",
+                 y = "Disapproval Rating")
+        ggplotly(p,originalData = T, tooltip = c("disapproval", "president", "party"), layer = 1)
+    })
+    
+    output$main_unsure_plot <- renderPlotly({
+        p <- ggplot(in_data(), aes(x = days_in_office, y = unsure, color = party)) +
+            stat_smooth(se = F, size = 3) +
+            stat_smooth(data = in_trump(), size = 3, se = F) +
+            scale_color_manual(values = c("Democrat" = "#232066", "Republican" = "#e91d0e", "Orange" = "orange")) +
+            theme(legend.position = "none") +
+            labs(x = "Days in Office",
+                 y = "Unsure Rating")
+        ggplotly(p,originalData = T, tooltip = c("unsure", "party"), layer = 1)
+    })
+    
+    output$second_unsure_plot <- renderPlotly({
+        p <- ggplot(in_proj(), aes(x = days_in_office, y = unsure, group = president, color = party)) +
+            stat_smooth(se = F, aes(linetype = line_type)) +
+            stat_smooth(data = in_trump(), se = F, size = 1, color = "orange") +
+            scale_color_manual(values = c("Democrat" = "#232066", "Republican" = "#e91d0e", "Orange" = "orange")) +
+            theme(legend.position = "none") +
+            labs(x = "Days in Office",
+                 y = "Unsure Rating")
+        ggplotly(p,originalData = T, tooltip = c("unsure", "president", "party"), layer = 1)
+    })
+}
+
+shinyApp(ui = ui, server = server)
+
---
+++ global.R
@@ -0,0 +1,168 @@
+# Scraper for http://www.presidency.ucsb.edu/data/popularity.php
+library(forcats)
+library(rvest)
+library(plotly)
+library(lubridate)
+library(tidyverse)
+library(magrittr)
+
+
+metaFiller <- function(vector) {
+    for (i in 1:length(vector)) {
+        if (!is.na(vector[i])) {
+            j <- vector[i]
+        }
+        if (is.na(vector[i])) {
+            vector[i] <- j
+        }
+    }
+    return(vector)
+}
+
+
+#### Parser ####
+
+# # works for a single webpage
+# site <- read_html("http://www.presidency.ucsb.edu/data/popularity.php")
+# pres_approval <- site %>% html_nodes("table") %>% extract2(11) %>%
+#     html_table(fill = T)
+# 
+# site2 <- read_html("http://www.presidency.ucsb.edu/data/popularity.php?pres=32&sort=time&direct=DESC&Submit=DISPLAY")
+# fdr_approval <- site2 %>% html_nodes("table") %>% extract2(11) %>%
+#     html_table(fill = T)
+# 
+# # id'd presidential number in url, want to loop on that
+# results_list <- list()
+# for (i in 32:45) {
+#     url <- paste0("http://www.presidency.ucsb.edu/data/popularity.php?pres=", i, "&sort=time&direct=DESC&Submit=DISPLAY")
+#     result <- url %>% read_html %>% html_nodes("table") %>% extract2(11) %>% html_table(fill = T)
+#     results_list[[i]] <- result
+# }
+# 
+# # results grabbed 2017/2/18
+# saveRDS(results_list, "www/results.RDS")
+
+#### Scrubber ####
+results_list <- readRDS("www/results.RDS")
+
+parsed <- results_list[32:45] %>% map(~ select(., 1:3,5:7)) %>%
+    map(~ magrittr::extract(., -c(1:6), )) %>%
+    map(~ set_colnames(., c("president", "start_date", "end_date", "approval", "disapproval", "unsure"))) %>%
+    bind_rows()
+
+parsed$president %<>% ifelse(nchar(.) < 2, NA, .) %>% metaFiller()
+
+cleaned <- parsed %>% mutate_at(vars(contains("date")), "mdy") %>%
+    mutate_at(vars(4:6), "as.numeric") %>%
+    mutate(president = as.factor(president) %>% fct_inorder)
+
+# natural time course
+ggplot(cleaned, aes(end_date, approval, color = president)) +
+    geom_path()
+
+
+#### Adder ####
+# add a scaled time variable for days in office
+added <- cleaned %>% group_by(president) %>%
+    mutate(days_in_office = end_date - min(end_date))
+
+# add party variable manually
+party_key <- c("d","d", "r", "d", "d", "r", "r", "d", "r", "r", "d", "r", "d", "o") %>%
+    set_names(unique(added$president))
+party_key %<>% gsub("r","Republican", .) %>% gsub("d", "Democrat", .) %>% gsub("^o", "Orange", .)
+added %<>% mutate(party = party_key[president])
+
+# need to account for Obama weight; 2800 obs vs 270 for GWB
+o <- filter(added, president == "Barack Obama") %>% arrange(days_in_office)
+added %<>% filter(president != "Barack Obama")
+o <- filter(o, rep(c(T,rep(F, 9)), length.out = nrow(o)))
+added %<>% rbind(o)
+
+# need to get year out for filter
+added$year <- strftime(added$end_date, "%Y")
+
+# add unique id by party for linetype
+added %<>% group_by(party) %>% mutate(line_type = as.character(as.numeric(factor(president)) + 1))
+
+# shiny app storage
+saveRDS(added, "www/data.RDS")
+
+
+
+#### Play ####
+# plots for play
+ggplot(added, aes(days_in_office, approval, color = president)) +
+    geom_path(alpha = .5)
+
+ggplot(added, aes(days_in_office, approval, color = president)) +
+    geom_path(alpha = .5) +
+    facet_wrap(~president)
+
+ggplot(added, aes(days_in_office, approval, color = president)) +
+    stat_smooth(se = F)
+ggplotly()
+
+
+
+
+# map out new data frame with loess projects values to alleviate stat_smooth low n woes
+test <- added %>% gather(measure, value, approval:unsure) %>%
+    mutate(pres_mes = paste(president, measure, sep = "_"))
+
+test_mods <- test %>% split(.$pres_mes) %>%
+    map(~ loess(value ~ days_in_office, data = .))
+dummy_data <- test %>% split(.$pres_mes) %>%
+    map(~ max(.$days_in_office)) %>%
+    map(~ seq(0, ., by = 1))
+response_data <- map2(test_mods, dummy_data, predict) %>%
+    map(as.data.frame) %>% map(~mutate(., days_in = seq(0, by = 1, length.out = nrow(.)))) %>%
+    bind_rows(.id = "pres_mes") %>%
+    set_colnames(c("pres_mes", "value", "days_in")) %>%
+    separate(pres_mes, c("president", "measure"), sep = "_") %>%
+    group_by(president) %>%
+    spread(measure, value) %>%
+    set_colnames(c("president", "days_in_office", "approval", "disapproval", "unsure"))
+
+start_date <- added %>% group_by(president, party, line_type) %>%
+    slice(which.min(start_date)) %>%
+    select(1:2)
+
+projected <- full_join(response_data, start_date) %>%
+    mutate(end_date = start_date + days(days_in_office),
+           year = strftime(end_date, "%Y"),
+           start_date = lag(end_date)) %>%
+    mutate_at(vars(3:5), funs(round(., 2)))
+
+saveRDS(projected, "www/proj.RDS")
+
+trump_mods <- filter(test, president == "Donald J. Trump") %>%
+    split(.$pres_mes) %>%
+    map(~ lm(value ~ days_in_office, data = .))
+trump_data <- filter(test, president == "Donald J. Trump") %>%
+    split(.$pres_mes) %>%
+    map(~ max(.$days_in_office)) %>%
+    map(~ seq(0, 100, by = 1)) %>%
+    map(as.data.frame) %>% map(~ set_colnames(.,"days_in_office"))
+response_trump <- map2(trump_mods, trump_data, predict) %>%
+    map(as.data.frame) %>% map(~mutate(., days_in = seq(0, by = 1, length.out = nrow(.)))) %>%
+    bind_rows(.id = "pres_mes") %>%
+    set_colnames(c("pres_mes", "value", "days_in")) %>%
+    separate(pres_mes, c("president", "measure"), sep = "_") %>%
+    group_by(president) %>%
+    spread(measure, value) %>%
+    set_colnames(c("president", "days_in_office", "approval", "disapproval", "unsure"))
+
+start_date2 <- filter(added, president == "Donald J. Trump") %>%
+    group_by(president, party, line_type) %>%
+    slice(which.min(start_date)) %>%
+    select(1:2)
+
+projected_trump <- full_join(response_trump, start_date2) %>%
+    mutate(end_date = start_date + days(days_in_office),
+           year = strftime(end_date, "%Y"),
+           start_date = lag(end_date)) %>%
+    mutate_at(vars(3:5), funs(round(., 2)))
+
+saveRDS(projected_trump, "www/projected_trump.RDS")
+
+
